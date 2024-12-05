80638fde2ac412d0dc73d5228bca4362978eef23
Ryan <ryan.yeh@gmail.com>
2024-12-05 14:59:32 -0500

    Updated load_coin_data() to add 'next_close' and 'next_return' columns

--- code/lib/helper.py
+++ code/lib/helper.py
@@ -22,6 +22,13 @@ def load_coin_data(data_file='../../data/crypto-markets.csv'):
     # sort the dataset by 'slug' and 'dates'
     df = df.sort_values(['slug', 'date'])
 
+    # add next closing price and next day return columns
+    df['next_close'] = df.groupby('slug')['close'].shift(-1)
+    df['next_return'] = (df['next_close'] - df['close']) / df['close']
+
+    # drop the last row of each coins as we don't have the next day's closing price
+    df = df.dropna()
+
     # reset the dataframe index
     df = df.reset_index(drop=True)
 
@@ -38,7 +45,7 @@ def filter_by_years(df, years=4):
 # Split dataset by specific date. By default it will split the data so there's
 # one year worth of data for the test dataset.
 #
-def split_train_test(df, date='2017-11-29'):
+def split_train_test(df, date='2017-11-28'):
     train = df.loc[df['date'] < date].reset_index(drop=True)
     test = df.loc[df['date'] >= date].reset_index(drop=True)
     return train, test
